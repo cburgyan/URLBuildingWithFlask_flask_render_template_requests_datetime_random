84eb1d35e285b3de83574426125dfe9456bf4b77
cburgyan <cburgyan@gmail.com>
2022-07-15 18:43:34 -0500

    add footer

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import random
+import datetime
 
 
 app = Flask(__name__)
@@ -13,7 +15,14 @@ def make_h1(function):
 @app.route('/')
 # @ make_h1
 def intro_page():
-    return render_template('index.html')
+    rand = random.randint(1, 10)
+    now = datetime.datetime.now()
+    return render_template('index.html', rand_num=rand, year=now.year)
+
+
+@app.route('/guess/<name>')
+def guess_age_and_gender():
+    return render_template('age_and_gender.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
