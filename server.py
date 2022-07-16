bb2ce78327e23adfd153e22c964855be1e357dac
cburgyan <cburgyan@gmail.com>
2022-07-15 19:08:51 -0500

    create age_and_gender.html

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 import random
 import datetime
+import requests
+import ast
 
 
 app = Flask(__name__)
@@ -21,8 +23,16 @@ def intro_page():
 
 
 @app.route('/guess/<name>')
-def guess_age_and_gender():
-    return render_template('age_and_gender.html')
+def guess_age_and_gender(name):
+    age_text = requests.get(url=f'https://api.agify.io/?name={name}').text
+    gender_text = requests.get(url=f'https://api.genderize.io/?name={name}').text
+    print(age_text)
+    age_dict = ast.literal_eval(age_text)
+    age = age_dict["age"]
+    print(gender_text)
+    gender_dict = ast.literal_eval(gender_text)
+    gender = gender_dict["gender"]
+    return render_template('age_and_gender.html', name=name.title(), age=age, gender=gender)
 
 if __name__ == '__main__':
     app.run(debug=True)
