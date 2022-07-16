fc3603ed5f1c6c1ecad4fa46c1d88181490937fc
cburgyan <cburgyan@gmail.com>
2022-07-15 19:19:53 -0500

    add gender and age guessing statements to webpage

--- server.py
+++ server.py
@@ -24,14 +24,22 @@ def intro_page():
 
 @app.route('/guess/<name>')
 def guess_age_and_gender(name):
-    age_text = requests.get(url=f'https://api.agify.io/?name={name}').text
-    gender_text = requests.get(url=f'https://api.genderize.io/?name={name}').text
-    print(age_text)
-    age_dict = ast.literal_eval(age_text)
-    age = age_dict["age"]
-    print(gender_text)
-    gender_dict = ast.literal_eval(gender_text)
-    gender = gender_dict["gender"]
+    try:
+        age_text = requests.get(url=f'https://api.agify.io/?name={name}')
+        # print(age_text)
+        age = age_text.json()["age"]
+    except Exception as error_message:
+        print(error_message)
+        age = "??"
+
+    try:
+        gender_text = requests.get(url=f'https://api.genderize.io/?name={name}')
+        # print(gender_text)
+        gender = gender_text.json()["gender"]
+    except Exception as error_message:
+        print(error_message)
+        gender = "??"
+
     return render_template('age_and_gender.html', name=name.title(), age=age, gender=gender)
 
 if __name__ == '__main__':
